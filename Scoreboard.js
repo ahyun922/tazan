c1686329ad5f098901b4afdfe4156f6a6829019c
ahyun922 <33361135+ahyun922@users.noreply.github.com>
2021-10-14 02:17:24 -0700

    Add files via upload

---
+++ Scoreboard.js
@@ -0,0 +1,17 @@
+class Scoreboard{
+    constructor(){
+        this.score = 0
+    }
+
+    render(){
+        fill(255)
+        stroke(255)
+        textSize(gameSettings.textSize)
+        text('earn it : ', gameSettings.textSize, gameSettings.textSize  + 10)
+        text(this.score, gameSettings.textSize  * 4.5, gameSettings.textSize  + 10)
+    }
+
+    update(newScore){
+        this.score = newScore
+    }
+}
